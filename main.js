2c3dfbc00d0cce140b78f073376f7f1765e303f5
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-05-31 14:19:07 +0900

    lesson7

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ window.onload = function(){
 
   var core = new Core(320, 320);
   core.preload('chara1.png');
-  core.fps = 15;
+  core.fps = 20;
   core.onload = function(){
     var bear = new Sprite(32, 32);
     bear.image = core.assets['chara1.png'];
@@ -12,11 +12,30 @@ window.onload = function(){
     bear.y = 0;
     this.frame = 1;
 
+    position_x = 0;
+    position_y = 0;
+
     bear.addEventListener('enterframe', function(){
       if (core.input.left) this.x -= 5;
       if (core.input.right) this.x += 5;
       if (core.input.up) this.y -= 5;
       if (core.input.down) this.y += 5;
+
+      if (position_x > this.x + 2){
+        this.x += 5;
+        this.frame = this.age % 3;
+      }else if(position_x < this.x - 2){
+        this.x -= 5;
+        this.frame = this.age % 3;
+      }
+      if (position_y > this.y + 2){
+        this.y += 5;
+        this.frame = this.age % 3;
+      }else if(position_y < this.y - 2){
+        this.y -= 5;
+        this.frame = this.age % 3;
+      }
+
     });
 
     bear.on('touchstart', function(){
@@ -24,11 +43,23 @@ window.onload = function(){
     });
 
     core.rootScene.on('touchstart', function(e){
-      bear.x = e.x;
-      bear.y = e.y;
+      position_x = e.x;
+      position_y = e.y;
+    });
+
+
+    var label = new Label();
+    label.x = 200;
+    label.y = 5;
+    label.color = 'red';
+    label.font = '14px "Arial"';
+    label.text = '0';
+    label.on('enterframe', function(){
+      label.text = (core.frame/core.fps).toFixed(2);
     });
 
     core.rootScene.addChild(bear);
+    core.rootScene.addChild(label);
   };
   core.start();
 
