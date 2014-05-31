6f423a3a7aa5dbfc09a749847e1ab04f1617f384
Shoya Ishimaru <shoya.ishimaru@gmail.com>
2014-05-31 13:55:52 +0900

    lesson5

--- main.js
+++ main.js
@@ -4,18 +4,20 @@ window.onload = function(){
 
   var core = new Core(320, 320);
   core.preload('chara1.png');
-  core.fps = 60;
+  core.fps = 15;
   core.onload = function(){
     var bear = new Sprite(32, 32);
     bear.image = core.assets['chara1.png'];
     bear.x = 0;
     bear.y = 0;
+    this.frame = 1;
 
     bear.addEventListener('enterframe', function(){
       this.x += 5;
+      this.frame = this.age % 3 + 5;
       if (this.x > 320) this.x = 0;
-      this.rotate(2);
-      this.scale(1.01, 1.01);
+      //this.rotate(2);
+      //this.scale(1.01, 1.01);
     });
 
     core.rootScene.addChild(bear);
